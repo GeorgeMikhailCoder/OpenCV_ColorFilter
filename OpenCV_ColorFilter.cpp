d2d634c80feff627ac6fa560ddb4b75a077e3723
GeorgeMikhailCoder <george.mikhail@yandex.ru>
2022-02-10 22:57:14 +0300

    init

---
+++ OpenCV_ColorFilter.cpp
@@ -0,0 +1,30 @@
+﻿// OpenCV_ColorFilter.cpp : Defines the entry point for the application.
+//
+
+#include "OpenCV_ColorFilter.h"
+#include<opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+
+int main()
+{
+	
+	Vec<double, 4> di; 
+	di << 1, 2, 3, 4;
+	cout << di << endl;
+	cout<<di.t() << endl;
+	cout << di * di.t() << endl;
+	cout << di * di.t() << endl;
+	Matx22d D;//= di * di.t();
+	D << 1, 0, 0, 1;
+	Matx22d U, S, Vt;
+	SVD().compute(D, S, U, Vt);
+	//  double v = Vt(4);
+	//  cout << v << endl;
+
+
+	system("pause");
+	return 0;
+}
