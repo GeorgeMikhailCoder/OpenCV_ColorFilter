69123f86f54c38e59313ee7bf8f5934c6d3bf69a
GeorgeMikhailCoder <george.mikhail@yandex.ru>
2022-02-11 19:48:26 +0300

    Add functions for histograms

--- OpenCV_ColorFilter.cpp
+++ OpenCV_ColorFilter.cpp
@@ -9,6 +9,9 @@ using namespace cv;
 
 //SVD::compute(A, S, U, Vt);
 
+
+
+
 Vec3f myDefV(const Mat& im)
 {
     Matx33f Dsum = Matx33f::zeros();
@@ -42,45 +45,109 @@ Vec3f myDefV(const Mat& im)
     int ind = S.at<float>(2) > porog ? 2 : S.at<float>(1) > porog ? 1 : S.at<float>(0) > porog ?0 : -1;
 
     Vec3f v = Vt(Range(ind,ind+1),Range::all());
-
+    
+    v = v / norm(v);
     
     return v;
 }
 
-void boundCylinder(const Mat& im )
+int histSize = 256;
+int hist_w = 512, hist_h = 256;
+Mat img2hist(Mat img)
 {
-    Mat hist;
+    vector<Mat> bgr_planes;
+    split(img, bgr_planes);
 
-    int histSize = 5;
-    int hist_w = 512, hist_h = 400;
+
     float range[] = { 0, 256 }; //the upper boundary is exclusive
     const float* histRange[] = { range };
     bool uniform = true, accumulate = false;
 
-    Mat gray;
-    cvtColor(im, gray, COLOR_RGB2GRAY);
+    Mat b_hist, g_hist, r_hist;
 
-    calcHist(&gray, 1, 0, Mat(), hist, 1, & histSize, histRange, uniform, accumulate);
-    normalize(hist, hist, 0, hist_h, NORM_MINMAX, -1, Mat());
-    cout << hist << endl;
+    calcHist(&bgr_planes[0], 1, 0, Mat(), b_hist, 1, &histSize, histRange, uniform, accumulate);
+    calcHist(&bgr_planes[1], 1, 0, Mat(), g_hist, 1, &histSize, histRange, uniform, accumulate);
+    calcHist(&bgr_planes[2], 1, 0, Mat(), r_hist, 1, &histSize, histRange, uniform, accumulate);
+
+
+
+    normalize(b_hist, b_hist, 0, hist_h, NORM_MINMAX, -1, Mat());
+    normalize(g_hist, g_hist, 0, hist_h, NORM_MINMAX, -1, Mat());
+    normalize(r_hist, r_hist, 0, hist_h, NORM_MINMAX, -1, Mat());
+
+    vector<Mat> rgb_hist = { r_hist, g_hist, b_hist };
+
+    Mat res;
+    merge(rgb_hist,res);
     
-    imshow("hist", hist);
-    waitKey(0);
+
+    return res;
 }
+
+Mat hist2mat(Mat rgb_hist)
+{
+    vector<Mat> rgb_planes;
+    split(rgb_hist, rgb_planes);
+
+    Mat b_hist, g_hist, r_hist;
+    r_hist = rgb_planes[0];
+    g_hist = rgb_planes[1];
+    b_hist = rgb_planes[2];
+
+
+    int bin_w = cvRound((double)hist_w / histSize);
+    Mat histImage(hist_h, hist_w, CV_8UC3, Scalar(0, 0, 0));
+
+    for (int i = 1; i < histSize; i++)
+    {
+        line(histImage, Point(bin_w * (i - 1), hist_h - cvRound(b_hist.at<float>(i - 1))),
+            Point(bin_w * (i), hist_h - cvRound(b_hist.at<float>(i))),
+            Scalar(255, 0, 0), 2, 8, 0);
+        line(histImage, Point(bin_w * (i - 1), hist_h - cvRound(g_hist.at<float>(i - 1))),
+            Point(bin_w * (i), hist_h - cvRound(g_hist.at<float>(i))),
+            Scalar(0, 255, 0), 2, 8, 0);
+        line(histImage, Point(bin_w * (i - 1), hist_h - cvRound(r_hist.at<float>(i - 1))),
+            Point(bin_w * (i), hist_h - cvRound(r_hist.at<float>(i))),
+            Scalar(0, 0, 255), 2, 8, 0);
+    }
+    return histImage;
+}
+
+
+
+Mat boundCylinder(const Mat& im, Vec3f v, Scalar p0)
+{
+    cout <<"p0 = "<< p0 << endl;
+    
+    Mat dMat = im - p0;
+    cout << dMat << endl;
+    Mat Tmass;
+    for (int y = 0; y < im.rows; y++)
+        for (int x = 0; x < im.cols; x++)
+        {
+            const Vec3b di = dMat.at<uchar>(y, x);
+            double ti = di.dot(v)/ norm(v);
+            Tmass.push_back(Scalar(ti));
+        }
+    cout << Tmass << endl;
+    cout << v << endl;
+    return Mat();
+}
+
 
 
 int main()
 {
-	Mat im(Size(2, 3), CV_8UC3, Scalar(1,3,2));
+	Mat im(Size(20, 30), CV_8UC3, Scalar(200,50,40));
+    imshow("im", im);
     
-    
-    cout << im << endl;
+    //cout << im << endl;
     Scalar p0 = mean(im);
+    p0 = Scalar(30, 40, 50);
     Vec3f v = myDefV(im);
-
-
-    boundCylinder(im);
-
+    Mat hist = boundCylinder(im,v,p0);
+    
+    
 	system("pause");
 	return 0;
 }
